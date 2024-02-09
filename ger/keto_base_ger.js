41a21c6651b3295c58e3e22a0383b8ed46e19183
erCZ263k <159558070+erCZ263k@users.noreply.github.com>
2024-02-09 15:05:43 -0300

    Add files via upload

---
+++ ger/keto_base_ger.js
@@ -0,0 +1,7 @@
+function repDef() {
+document.body.innerHTML = document.body.innerHTML
+.replaceAll("[ProductName]", "Keto Base")
+.replaceAll("[GuaranteeDays]", "60")
+.replaceAll("[AffLink]", "www.fasttrack03.com/NMZW45XT/28JD3CWC/?")
+.replaceAll("[DiscountValue]", "46%")
+} repDef();
