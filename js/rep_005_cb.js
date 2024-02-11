ec6d99ca8b8294dfa094b8a0c2d687cb294e5b94
erCZ263k <159558070+erCZ263k@users.noreply.github.com>
2024-02-11 08:31:37 -0300

    Update rep_005_cb.js

--- js/rep_005_cb.js
+++ js/rep_005_cb.js
@@ -1,4 +1,14 @@
 function repDefault() {
 document.body.innerHTML = document.body.innerHTML
+.replaceAll("[TopBut]", "&tid=topbut_")
+.replaceAll("[FixedBut]", "&sub5=fixedbut")
+.replaceAll("[SecBut]", "&tid=secbut_")
+.replaceAll("[ThiBut]", "&tid=thibut_")
+.replaceAll("[GuaBut]", "&tid=guabut_")
 .replaceAll("[MainColor]", "#F19220")
+.replaceAll("[Pack1]", "&tid=pack1_")
+.replaceAll("[Pack2]", "&tid=pack2_")
+.replaceAll("[Pack3]", "&tid=pack3_")
+.replaceAll("[Pack4]", "&tid=pack4_")
+.replaceAll("[Pack5]", "&tid=pack5_")
 } repDefault();
