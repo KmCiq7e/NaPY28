5446e9c7affc90e4f883e6236b1460b6a1f667ee
erCZ263k <159558070+erCZ263k@users.noreply.github.com>
2024-02-10 22:38:11 -0300

    Create rep_003_bg.js

---
+++ js/rep_003_bg.js
@@ -0,0 +1,14 @@
+function repDefault() {
+document.body.innerHTML = document.body.innerHTML
+.replaceAll("[TopBut]", "&subid5=topbut")
+.replaceAll("[FixedBut]", "&subid5=fixedbut")
+.replaceAll("[SecBut]", "&subid5=secbut")
+.replaceAll("[ThiBut]", "&subid5=thibut")
+.replaceAll("[GuaBut]", "&subid5=guabut")
+.replaceAll("[MainColor]", "#F19220")
+.replaceAll("[Pack1]", "&subid5=pack1")
+.replaceAll("[Pack2]", "&subid5=pack2")
+.replaceAll("[Pack3]", "&subid5=pack3")
+.replaceAll("[Pack4]", "&subid5=pack4")
+.replaceAll("[Pack5]", "&subid5=pack5")
+} repDefault();
