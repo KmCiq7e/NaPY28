9b7857c792fa23b4af48d17f33bf333b6697b77d
erCZ263k <159558070+erCZ263k@users.noreply.github.com>
2024-02-29 11:49:27 -0300

    Update rep_003_cb.js

--- js/rep_003_cb.js
+++ js/rep_003_cb.js
@@ -1,4 +1,9 @@
+var getdayNames = new Array("Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday");
+var getdayMonth = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+var getNow = new Date();
+var dayOfTheWeek = getNow.getDay();
 function repDefault() {
+document.getElementById("year1").innerHTML = new Date().getFullYear();
 document.body.innerHTML = document.body.innerHTML
 .replaceAll("[MainColor]", "#F19220")
 } repDefault();
