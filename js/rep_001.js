a059966f8acd8aea1a240a029dd6dc31853b2ae4
erCZ263k <159558070+erCZ263k@users.noreply.github.com>
2024-02-29 11:47:20 -0300

    Update rep_001.js

--- js/rep_001.js
+++ js/rep_001.js
@@ -2,6 +2,7 @@ var getdayNames = new Array("Sunday", "Monday", "Tuesday", "Wednesday","Thursday
 var getdayMonth = new Array("January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December");
 var getNow = new Date();
 var dayOfTheWeek = getNow.getDay();
+document.getElementById("year1").innerHTML = new Date().getFullYear();
 function repDefault() {
 document.body.innerHTML = document.body.innerHTML
 .replaceAll("[TopBut]", "&sub5=topbut")
