a9f5e57e5fcf1e813d98b47d103501187b2a982c
KmCiq7e <168497559+KmCiq7e@users.noreply.github.com>
2024-05-02 13:57:38 -0300

    Update rep_eng.js

--- js/eng/rep_eng.js
+++ js/eng/rep_eng.js
@@ -1,3 +1,1 @@
-function rep_Eng() {
-document.getElementById("today").innerHTML = getdayNames[(getNow.getDay())] + ", " + getdayMonth[(getNow.getMonth())] + " " + getNow.getDate();
-} rep_Eng();
+document.getElementById("today").innerHTML = getdayMonth[(getNow.getMonth())] + " " + getNow.getDate() + ", " + thisYear;
