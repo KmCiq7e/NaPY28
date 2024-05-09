afbcf59edf4194fab96fa4855d555bd987267bd6
KmCiq7e <168497559+KmCiq7e@users.noreply.github.com>
2024-05-08 21:39:34 -0300

    Delete js/icon.js

--- js/icon.js
+++
@@ -1,7 +0,0 @@
-var iconx = document.querySelector("iconx[rel~='icon']"); 
-if (!iconx) { 
-  iconx = document.createElement('iconx'); 
-  iconx.rel = 'icon'; 
-  document.getElementsByTagName('head')[0].appendChild(iconx); 
-} 
-iconx.href = "https://[Icon]";
