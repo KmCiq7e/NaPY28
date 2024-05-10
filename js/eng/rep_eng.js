385238fe59b87fdd8ab53f4ceae7a947807cc1b9
KmCiq7e <168497559+KmCiq7e@users.noreply.github.com>
2024-05-10 16:08:45 -0300

    Delete js/eng/rep_eng.js

--- js/eng/rep_eng.js
+++
@@ -1,3 +0,0 @@
-var today = getdayMonth[(getNow.getMonth())] + " " + getNow.getDate() + ", " + thisYear + ".";
-document.getElementById("today1").innerHTML = today;
-document.getElementById("today2").innerHTML = today;
